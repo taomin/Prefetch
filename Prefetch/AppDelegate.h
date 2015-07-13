822b5b8690a4e73c6f5abbe011787e5c7e0c4603
taomin <itiachang@gmail.com>
2015-07-13 12:09:19 -0700

    Initial Commit

---
+++ Prefetch/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Prefetch
+//
+//  Created by Taomin Chang on 7/13/15.
+//  Copyright (c) 2015 Taomin Chang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Prefetch/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Prefetch
+//
+//  Created by Taomin Chang on 7/13/15.
+//  Copyright (c) 2015 Taomin Chang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
